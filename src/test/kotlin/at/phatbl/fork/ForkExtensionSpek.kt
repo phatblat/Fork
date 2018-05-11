faa829c92cb11b6c7aad667f6586e7fcabaa1c05
Ben Chatelain <ben@octop.ad>
2018-05-11 09:29:19 -0600

    📋 Add upstream property to extension

--- src/test/kotlin/at/phatbl/fork/ForkExtensionSpek.kt
+++ src/test/kotlin/at/phatbl/fork/ForkExtensionSpek.kt
@@ -1,6 +1,15 @@
 package at.phatbl.fork
 
 import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import kotlin.test.assertNull
 
 object ForkExtensionSpek : Spek({
+    describe("dsl extension") {
+        val extension = ForkExtension()
+        it("has an upstream property") {
+            assertNull(extension.upstream)
+        }
+    }
 })
